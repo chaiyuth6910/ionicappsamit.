fd014b51ce73624d5324d5be074c63da2929b4c2
Samit Koyom <samitkoyom@gmail.com>
2018-01-26 09:39:01 +0700

    update project ionicapp

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DetailPage } from '../detail/detail';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,12 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  // ฟังก์ชันเปิดหน้าใหม่จากการคลิ๊กปุ่มอ่านต่อ
+  showdetail()
+  {
+    this.navCtrl.push(DetailPage);
   }
 
 }
